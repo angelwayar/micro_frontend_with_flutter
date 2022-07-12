0884d897e26c6d9ffc9ca6667a3cb6b7340cd4be
angel wayar encinas <angelmanuelwayar@gmail.com>
2022-07-11 20:33:01 -0400

    Agregando vistas, inyecciones y manejador de estado

---
+++ login_module/lib/login/utils/injection.util.dart
@@ -0,0 +1,32 @@
+import 'package:get_it/get_it.dart';
+import 'package:login_module/login/data/datasources/remote.datasource.dart';
+import 'package:login_module/login/data/datasources/remote_imp.datasource.dart';
+import 'package:login_module/login/data/repositories/remote_imp.repository.dart';
+import 'package:login_module/login/domain/repositories/remote.repository.dart';
+import 'package:login_module/login/domain/usecases/login_with_credentials.usecase.dart';
+import 'package:login_module/login/presentation/cubit/login_cubit.dart';
+
+class InjectionLogin {
+  final getIt = GetIt.instance;
+
+  void initInjectionLogin() {
+    /// Data Source
+    getIt.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImp());
+
+    /// Repository
+    getIt.registerLazySingleton<RemoteRepository>(
+      () => RemoteRepositoryImp(
+        remoteDataSource: getIt.get<RemoteDataSource>(),
+      ),
+    );
+
+    /// Bloc
+    getIt.registerFactory(
+      () => LoginCubit(
+        loginWithCredentialsUseCase: LoginWithCredentialsUseCase(
+          remoteRepository: getIt.get<RemoteRepository>(),
+        ),
+      ),
+    );
+  }
+}
---
+++ login_module/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:login_module/app.dart';
+
+void main() {
+  runApp(const LoginApp());
+}
